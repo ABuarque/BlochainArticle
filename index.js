3bbe3a202209e144ce365be4d502ad921dfd7ff9
ABuarque <abuarquemf@gmail.com>
2018-07-25 21:10:10 -0300

    change

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import BlockChain from "./BlockChain";
 import Block from "./Block";
 import Transaction from "./Transaction";
 
-let coolChain = new BlockChain(2, 100);
-coolChain.createTransaction(new Transaction(500, 40, 100));
-coolChain.createTransaction(new Transaction(500, 40, 150));
+let fancyChain = new BlockChain(2, 100);
+fancyChain.createTransaction(new Transaction(500, 40, 100));
+fancyChain.createTransaction(new Transaction(500, 40, 150));
 
 console.log('\n Starting the miner...');
-coolChain.minePendingTransactions(31);
+fancyChain.minePendingTransactions(31);
 
-console.log('\nBalance of miner 31 is', coolChain.getBalanceOfId(31));
+console.log('\nBalance of miner 31 is', fancyChain.getBalanceOfId(31));
 
 console.log('\n Starting the miner again...');
-coolChain.minePendingTransactions(31);
+fancyChain.minePendingTransactions(31);
 
-console.log('\nBalance of miner 31 is', coolChain.getBalanceOfId(31));
+console.log('\nBalance of miner 31 is', fancyChain.getBalanceOfId(31));
